9858fc1979b75997d54f68f6ff7189a5a6c96615
Daniel Hernandez <daniel@poliwin.mx>
2025-09-04 12:01:50 -0600

    Inserción de seed registro a registro.

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response-interface';
-import { log } from 'console';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=1',
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    for (const { name, url } of data.results) {
       console.log({ name, url });
 
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      log({ no, name });
-    });
-    return data.results;
+      await this.pokemonModel.create({ name, no });
+    }
+    return 'seed executed';
   }
 }
