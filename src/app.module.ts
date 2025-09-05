14c9aa8746fa8383a56f0f2c7f7da6f90ca0f6c6
Daniel Hernandez <daniel@poliwin.mx>
2025-09-05 17:10:52 -0600

    Preparando archivo .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,15 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(
-      // 'mongodb://admin:secret@localhost:27017/nest-pokedex?authSource=admin',
-      'mongodb+srv://daniel_pokedex:kIMeSjGyPCGAzLXk@nestpokedex.edmk1vx.mongodb.net/?retryWrites=true&w=majority&appName=nestpokedex',
-    ),
+    MongooseModule.forRoot(process.env.MONGODB!),
     PokemonModule,
     CommonModule,
     SeedModule,
